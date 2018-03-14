6b445b97b791d5882ba45a0b19695018966d1694
MKonos <Mihalis4566@gmail.com>
2018-03-14 08:05:57 -0500

    Score increment and score display

---
+++ Assets/Scripts/ScoreCount.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour
+{
+    [SerializeField]
+    public Text scoreDisplay;
+
+    void Update()
+    {
+        int sco = Bomb.score;
+        scoreDisplay.text = sco.ToString();
+
+        if (sco >= 5)
+        {
+            scoreDisplay.text = "You Win!";
+        }
+    }
+}
