d19c949de0ff777c2b5a6bbe02ea559658ca90f2
MKonos <Mihalis4566@gmail.com>
2018-03-14 07:51:46 -0500

    bomb script

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Material[] material;
+    Renderer rend;
+    public static int score;
+
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    void OnCollisionEnter()
+    {
+        if (rend.sharedMaterial != material[1])
+        {
+            score += 1;
+        }
+
+        rend.sharedMaterial = material[1];
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
