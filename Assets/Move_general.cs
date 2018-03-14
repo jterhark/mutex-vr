4b22419a953525d189d78ab1d12a14b5f639e617
m-betancourt2 <mbetan4@uic.edu>
2018-03-13 21:42:39 -0500

    update to general
    Short scale version of soldier filed. General can move around table using the a and d keys. Table use as example, will be substitute later

---
+++ Assets/Move_general.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_general : MonoBehaviour {
+
+    float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal"));
+    }
+}
