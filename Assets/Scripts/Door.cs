9c4c96b3410eb65a6e4c07eac368e1bafc3cb135
m-betancourt2 <mbetan4@uic.edu>
2018-03-21 01:34:06 -0500

    Door and Key

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    Pick_up key;
+
+
+    void Start()
+    {
+        key = GameObject.Find("key_gold").GetComponent<Pick_up>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (key.i == 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
