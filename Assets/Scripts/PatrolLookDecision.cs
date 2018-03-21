7a0a57eef6082c12cd75cfa0d947733fb36e0edd
Jake TerHark <jterha2@uic.edu>
2018-03-21 03:29:03 -0500

    Navmash / FSM / Patrol

---
+++ Assets/Scripts/PatrolLookDecision.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Decisions/Look")]
+public class PatrolLookDecision : Decision {
+    public override bool Decide(StateController controller) {
+        Console.WriteLine("IN LOOK DECISION");
+        return Look(controller);
+    }
+
+    private bool Look(StateController controller) {
+        RaycastHit hit;
+        
+        Debug.DrawRay(controller.eyes.position, controller.eyes.forward.normalized * 15.0f, Color.green);
+        
+        if (Physics.SphereCast(controller.eyes.position, 2.0f, controller.eyes.forward, out hit, 15.0f) &&
+            hit.collider.CompareTag("Soldier")) {
+            controller.target = hit.transform;
+            return true;
+        }
+
+        return false;
+    }
+}
