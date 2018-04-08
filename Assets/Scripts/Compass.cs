198be7e5ef8b67adc5427fdb3899d12104a28ee5
m-betancourt2 <mbetan4@uic.edu>
2018-04-08 12:53:26 -0500

    river update
    river slows down working speed, therefore it was made a prefab to just insert it when everything its ready.

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour {
+
+    public Transform player;
+    public Texture compBg;
+    public Texture blipTex;
+
+    void OnGUI()
+    {
+        //x position, y position, 120,120(120 are for image)
+        GUI.DrawTexture(new Rect(0, 300, 120, 120), compBg);
+        GUI.DrawTexture(CreateBlip(), blipTex);
+    }
+
+    Rect CreateBlip()
+    {
+        float angDeg = player.eulerAngles.y - 90;
+        float angRed = angDeg * Mathf.Deg2Rad;
+
+        float blipX = 25 * Mathf.Cos(angRed);
+        float blipY = 25 * Mathf.Sin(angRed);
+
+        //positions for path x and y
+        blipX += 55;
+        blipY += 355;
+
+        return new Rect(blipX, blipY, 10, 10);
+    }
+}
