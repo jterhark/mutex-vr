82e95ab55b7cfc84a135b9d6f0bc90a8e07029cf
MKonos <Mihalis4566@gmail.com>
2018-04-09 00:22:33 -0500

    health system introduction (press e to kill urself ;D)

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+public class HealthSystem : MonoBehaviour {
+
+	[SerializeField]
+    public Text scoreDisplay;
+	
+	[SerializeField]
+	public Transform player;
+	
+	[SerializeField]
+	public Transform respawnPoint;
+
+	int _curHealh = 100;
+	int _maxHealth = 100;
+	string _healthtext;
+
+	void Start ()
+	{	
+	}
+
+	void Update()
+	{
+		_healthtext = _curHealh.ToString() + " / " + _maxHealth.ToString();
+
+		if (_curHealh < 0)
+		{
+			_curHealh = 0;
+		}
+
+		if (_curHealh > 100)
+		{
+			_curHealh = 100;
+		}
+
+		if (Input.GetKeyDown("e"))
+		{
+			_curHealh -= 10;
+		}
+
+
+		if (_curHealh == 0)
+		{
+			player.transform.position = respawnPoint.transform.position;
+			_curHealh = 100;
+		}
+
+	scoreDisplay.text = _healthtext;
+	}
+
+}
