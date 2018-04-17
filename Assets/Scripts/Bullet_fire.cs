d4cdc686f6c2145ba1203c60a1574709583a4cc0
m-betancourt2 <mbetan4@uic.edu>
2018-04-16 23:50:56 -0500

    add sensors to tanks

--- Assets/Scripts/Bullet_fire.cs
+++ Assets/Scripts/Bullet_fire.cs
@@ -7,35 +7,40 @@ public class Bullet_fire : MonoBehaviour {
 
     public GameObject bullet;
     public Transform position;
+    public float range;
+    public Transform player;
     
     float start = 0.0f;
-    float finish = 120.0f;
+    float finish = 50.0f;
   //  public Transform bulletSpawn;
 	// Use this for initialization
 	void Start () {
-		
+        range = 30.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (start==0.0)
+        if (Vector3.Distance(player.position, transform.position) <= range)
         {
-            var fire = (GameObject)Instantiate(bullet, position.position, position.rotation);
-            
-            fire.GetComponent<Rigidbody>().velocity = fire.transform.forward * 6;
-            Destroy(fire, 5.0f);
-            //Debug.Log("here");
-            start++;
-        }
-        else if (start == finish)
-        {
-            start = 0.0f;
-         //   Debug.Log("equal");
-        }
-        else
-        {
-            start++;
-           // Debug.Log("sum");
+            if (start == 0.0)
+            {
+                var fire = (GameObject)Instantiate(bullet, position.position, position.rotation);
+
+                fire.GetComponent<Rigidbody>().velocity = fire.transform.forward * 6;
+                Destroy(fire, 5.0f);
+                //Debug.Log("here");
+                start++;
+            }
+            else if (start == finish)
+            {
+                start = 0.0f;
+                //   Debug.Log("equal");
+            }
+            else
+            {
+                start++;
+                // Debug.Log("sum");
+            }
         }
       
 	}
