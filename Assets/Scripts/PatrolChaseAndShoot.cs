78afcf8817fcf9b17d0aa0ace186712296f2da27
Jake TerHark <jterha2@uic.edu>
2018-04-17 23:55:49 -0500

    Chase Tank

--- Assets/Scripts/PatrolChaseAndShoot.cs
+++ Assets/Scripts/PatrolChaseAndShoot.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu (menuName = "AI/Actions/Chase And Shoot")]
 public class PatrolChaseAndShoot : Action {
+
+	private float shoot = 0.0f; 
+	
 	public override void Act(StateController controller) {
 		Chase(controller);
 	}
@@ -13,15 +17,21 @@ public class PatrolChaseAndShoot : Action {
 		controller.agent.Resume();
 
 		RaycastHit hit;
-		
-		if (Physics.SphereCast(controller.eyes.position, 2.0f, controller.eyes.forward, out hit, 15.0f) &&
-		    hit.collider.CompareTag("Player")) {
-			var bullet = Instantiate(controller.BulletPrefab, controller.BulletSpawnPoint.position, new Quaternion(0, 0, 0, 0));
-			bullet.transform.LookAt(hit.transform);
-			var rb = bullet.GetComponent<Rigidbody>();
-			rb.velocity = new Vector3(0,10,0);
+
+		Debug.Log(shoot);
+		Debug.Log(Time.time);
+		if (shoot < Time.time) {
+			Debug.Log("In");
+			if (Physics.SphereCast(controller.eyes.position, 2.0f, controller.eyes.forward, out hit, 20.0f) &&
+			    hit.collider.CompareTag("Player")) {
+				var bullet = Instantiate(controller.BulletPrefab, controller.BulletSpawnPoint.position, new Quaternion(0, 0, 0, 0));
+				bullet.transform.LookAt(hit.transform);
+				var rb = bullet.GetComponent<Rigidbody>();
+				rb.velocity = new Vector3(10, 0, 0);
+				shoot = Time.time + 1;
+			}
 		}
 
-		
+
 	}
 }
