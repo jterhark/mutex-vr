16ccbdfb15b7e958db51aeb11972154356f3a9e4
Jake TerHark <jterha2@uic.edu>
2018-04-18 00:17:25 -0500

    Chase and Shoot Tank Ready

--- Assets/Scripts/PatrolChaseAndShoot.cs
+++ Assets/Scripts/PatrolChaseAndShoot.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 [CreateAssetMenu (menuName = "AI/Actions/Chase And Shoot")]
 public class PatrolChaseAndShoot : Action {
 
-	private float shoot = 0.0f; 
-	
+	private float shoot;
+
+
 	public override void Act(StateController controller) {
 		Chase(controller);
 	}
@@ -18,16 +19,17 @@ public class PatrolChaseAndShoot : Action {
 
 		RaycastHit hit;
 
-		Debug.Log(shoot);
-		Debug.Log(Time.time);
+		if (shoot > (Time.time + 5.0f)) {
+			shoot = 0.0f;
+		}
+
 		if (shoot < Time.time) {
-			Debug.Log("In");
 			if (Physics.SphereCast(controller.eyes.position, 2.0f, controller.eyes.forward, out hit, 20.0f) &&
 			    hit.collider.CompareTag("Player")) {
 				var bullet = Instantiate(controller.BulletPrefab, controller.BulletSpawnPoint.position, new Quaternion(0, 0, 0, 0));
 				bullet.transform.LookAt(hit.transform);
 				var rb = bullet.GetComponent<Rigidbody>();
-				rb.velocity = new Vector3(10, 0, 0);
+				rb.velocity = (hit.transform.position - bullet.transform.position).normalized * 20;
 				shoot = Time.time + 1;
 			}
 		}
