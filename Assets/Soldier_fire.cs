7133095136df5fb167e38d73195c52366857ac48
m-betancourt2 <mbetan4@uic.edu>
2018-04-28 20:03:57 -0500

    Added Soldier Enemy

---
+++ Assets/Soldier_fire.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier_fire : MonoBehaviour
+{
+
+
+    public GameObject bullet;
+    public Transform position;
+    public float range;
+    public Transform player;
+
+    float start = 0.0f;
+    float finish = 50.0f;
+
+    // public Transform bulletSpawn;
+    // Use this for initialization
+    void Start()
+    {
+        range = 18.0f;
+    }
+
+    void OnCollisionEnter()
+    {
+        HealthSystem._curHealh -= 20;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.position, transform.position) <= range)
+        {
+            if (start == 0.0)
+            {
+                var fire = (GameObject)Instantiate(bullet, position.position, position.rotation);
+
+                fire.GetComponent<Rigidbody>().velocity = fire.transform.forward * 10;
+                Destroy(fire, 5.0f);
+                //Debug.Log("here");
+
+                start++;
+            }
+            else if (start == finish)
+            {
+                start = 0.0f;
+                //   Debug.Log("equal");
+            }
+            else
+            {
+                start++;
+                // Debug.Log("sum");
+            }
+
+        }
+
+    }
+
+
+}
---
+++ Assets/TESTE.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTE : MonoBehaviour {
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 10;
+    float MinDist = 0.5f;
+    public float range;
+    public Transform player;
+
+    Animator anim;
+    // Use this for initialization
+    void Start () {
+        range = 20;
+	}
+
+    void Awake()
+    {
+        // Set up the reference.
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if ((Vector3.Distance(player.position, transform.position) <= range))
+        {
+            transform.LookAt(Player);
+
+            if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+            {
+                anim.ResetTrigger("Stay");
+                anim.SetTrigger("Run");
+                transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+                if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+                {
+                    //Here Call any function U want Like Shoot at here or something
+                }
+
+            }
+
+
+        }
+        else
+        {
+              anim.ResetTrigger("Run");
+              anim.SetTrigger("Stay");
+        }
+
+
+    }
+}
