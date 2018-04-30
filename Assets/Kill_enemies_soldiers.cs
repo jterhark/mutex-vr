4b79dc4c985ead4dbf11851bba0206664c56b8a1
m-betancourt2 <mbetan4@uic.edu>
2018-04-30 18:56:51 -0500

    Latest update with more enemies and a fountain showing mugen's work

---
+++ Assets/Kill_enemies_soldiers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill_enemies_soldiers : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("gun_bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Soldier_sniper.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier_sniper : MonoBehaviour
+{
+
+
+    public GameObject bullet;
+    public Transform position;
+    public float range;
+    public Transform player;
+
+    float start = 0.0f;
+    float finish = 50.0f;
+
+    // public Transform bulletSpawn;
+    // Use this for initialization
+    void Start()
+    {
+        range = 50.0f;
+    }
+
+    void OnCollisionEnter()
+    {
+        HealthSystem._curHealh -= 20;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.position, transform.position) <= range)
+        {
+            transform.LookAt(player);
+            if (start == 0.0)
+            {
+                var fire = (GameObject)Instantiate(bullet, position.position, position.rotation);
+
+                fire.GetComponent<Rigidbody>().velocity = fire.transform.forward * 10;
+                Destroy(fire, 5.0f);
+                //Debug.Log("here");
+
+                start++;
+            }
+            else if (start == finish)
+            {
+                start = 0.0f;
+                //   Debug.Log("equal");
+            }
+            else
+            {
+                start++;
+                // Debug.Log("sum");
+            }
+
+        }
+
+    }
+
+
+}
